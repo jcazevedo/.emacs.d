0b88833b7ba0873e0cb6869b08de39c53b2b3a51
Joao Azevedo <joao.c.azevedo@gmail.com>
2014-12-16 01:09:34 +0000

    Enable helm find-file completion with TAB

--- modules/jcazevedo-helm.el
+++ modules/jcazevedo-helm.el
@@ -14,13 +14,15 @@
       helm-buffers-fuzzy-matching           t
       helm-move-to-line-cycle-in-source     t
       helm-ff-search-library-in-sexp        t
+      helm-ff-auto-update-initial-value     nil
       helm-ff-file-name-history-use-recentf t)
 
 (global-set-key (kbd "C-c h") 'helm-command-prefix)
 (global-unset-key (kbd "C-x c"))
 
-(define-key helm-command-map (kbd "o") 'helm-occur)
-(define-key helm-command-map (kbd "g") 'helm-do-grep)
+(define-key helm-command-map   (kbd "o")   'helm-occur)
+(define-key helm-command-map   (kbd "g")   'helm-do-grep)
+(define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action)
 
 (helm-mode 1)
 
