a83e56b3af1faeae423550edf15f0085ff864028
Joao Azevedo <joao.c.azevedo@gmail.com>
2014-12-22 18:53:43 +0000

    Fix package name

--- core/jcazevedo-twitter.el
+++ core/jcazevedo-twitter.el
@@ -1,8 +1,9 @@
 (jcazevedo-require-packages '(twittering-mode))
 
 (setq
+ twittering-use-master-password t
  twittering-icon-mode t
  twittering-use-icon-storage t
  twittering-number-of-tweets-on-retrieval 200)
 
-(provide 'jcazevedo-twitter.el)
+(provide 'jcazevedo-twitter)
