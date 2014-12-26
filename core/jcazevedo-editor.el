8d44ad6f81ccf45de89eaf4e6b8681c0fa23f1cf
Joao Azevedo <joao.c.azevedo@gmail.com>
2014-12-26 17:20:11 +0000

    Enable smartparens mode

--- core/jcazevedo-editor.el
+++ core/jcazevedo-editor.el
@@ -19,6 +19,15 @@
 ;; Revert buffers automatically when underlying files are changed externally
 (global-auto-revert-mode t)
 
+;; smart pairing for all
+(require 'smartparens-config)
+(setq sp-base-key-bindings 'paredit)
+(setq sp-autoskip-closing-pair 'always)
+(setq sp-hybrid-kill-entire-symbol nil)
+(sp-use-paredit-bindings)
+
+(show-smartparens-global-mode +1)
+
 ;; Meaningful names for buffers with the same name
 (require 'uniquify)
 (setq uniquify-buffer-name-style 'forward)
