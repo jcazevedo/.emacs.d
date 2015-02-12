6ffbc1dfc30fff9398998915d8db24d178d66ee7
Joao Azevedo <joao.c.azevedo@gmail.com>
2015-02-12 00:22:28 +0000

    Add org settings for clocking

--- modules/jcazevedo-org.el
+++ modules/jcazevedo-org.el
@@ -11,10 +11,36 @@
 (add-hook 'org-mode-hook (lambda () (run-hooks 'jcazevedo/org-mode-hook)))
 
 (setq
+ org-directory "org/"
  org-agenda-files (list "~/org")
  org-default-notes-file "~/org/refile.org"
  org-mobile-directory "~/Dropbox/MobileOrg"
+ org-todo-keywords (quote ((sequence "TODO(t)" "STARTED(s)" "ONGOING(o)" "|" "DONE(d)" "CANCELLED(c)" "MEETING(m)")))
+ org-todo-keyword-faces (quote (("TODO" :foreground "red" :weight bold)
+                                ("STARTED" :foreground "dodger blue" :weight bold)
+                                ("ONGOING" :foreground "dodger blue" :weight bold)
+                                ("DONE" :foreground "forest green" :weight bold)
+                                ("CANCELLED" :foreground "forest green" :weight bold)
+                                ("MEETING" :foreground "forest green" :weight bold)))
  org-use-fast-todo-selection t
- org-log-done 'time)
+ org-treat-S-cursor-todo-selection-as-state-change nil
+ org-log-done nil
+ org-capture-templates (quote (("t" "todo" entry (file "~/org/refile.org")
+                                "* TODO %?\n%U\n%a\n" :clock-in t :clock-resume t)
+                               ("m" "Meeting" entry (file "~/org/refile.org")
+                                "* MEETING with %? :MEETING:\n%U" :clock-in t :clock-resume t)))
+ org-drawers (quote ("PROPERTIES" "LOGBOOK"))
+ org-clock-in-resume t
+ org-clock-into-drawer t
+ org-clock-out-remove-zero-time-clocks t
+ org-clock-persist t
+ org-clock-persist-query-resume nil
+ org-clock-auto-clock-resolution (quote when-no-clock-is-running)
+ org-clock-report-include-clocking-task t
+ org-agenda-clockreport-parameter-plist (quote (:link t :maxlevel 5 :fileskip0 t :compact t :narrow 80))
+ org-columns-default-format "%40ITEM %TODO %5Effort(Effort){:} %6CLOCKSUM"
+ org-clock-out-when-done nil)
+
+(org-clock-persistence-insinuate)
 
 (provide 'jcazevedo-org)
