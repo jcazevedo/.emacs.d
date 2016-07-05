3d0ea541f0fdbf0eceb13f990bc32e2f777276c9
Joao Azevedo <joao.c.azevedo@gmail.com>
2016-07-05 11:52:49 +0100

    Use marked as the markdown command

--- modules/jcazevedo-markdown.el
+++ modules/jcazevedo-markdown.el
@@ -1,6 +1,8 @@
 (use-package markdown-mode
   :ensure t
   :mode "\\.markdown\\'" "\\.md\\'"
+  :init
+  (setq markdown-command "marked --gfm")
   :config
   (use-package markdown-toc
     :ensure t))
