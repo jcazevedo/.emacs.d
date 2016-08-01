04e556b218fec132e7c0b6b4f6b77116969e99d9
Joao Azevedo <joao.c.azevedo@gmail.com>
2016-08-01 01:34:41 +0100

    Add programming mode hooks to r-mode

--- modules/jcazevedo-r.el
+++ modules/jcazevedo-r.el
@@ -1,5 +1,9 @@
 (use-package ess
   :ensure t
+  :init
+  (add-hook 'ess-mode-hook 'jcazevedo/enable-whitespace-mode)
+  (add-hook 'ess-mode-hook 'smartparens-mode)
+  (add-hook 'ess-mode-hook 'jcazevedo/enable-linum-mode)
   :config
   (require 'ess-site))
 
